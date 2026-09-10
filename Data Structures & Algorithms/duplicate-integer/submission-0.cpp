a5834c907ce5ecc5cbed3fc1a819b7324f710f74
Ravi Kumar Singh <nravisingh70@gmail.com>
2026-09-10 19:29:17 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        map<long long , int> mp;
+
+        for(int i = 0 ; i < n ; i++){
+            mp[nums[i]]++;
+        }
+
+        for(auto it : mp){
+            if (it.second > 1) return true;
+        }
+        return false;
+        
+    }
+};
